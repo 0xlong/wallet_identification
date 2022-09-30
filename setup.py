3fe72ceb0b70f698a79ddaff0a3f9b22cd80392c
Longin_Imperial <longin.le18@imperial.ac.uk>
2022-09-30 14:46:52 +0200

    Project set-up commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for analysing blockchain addresses (wallets).',
+    author='0xlong',
+    license='MIT',
+)
